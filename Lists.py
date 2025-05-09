1325755343d2cf073c1a0a7de75f52d17f6d7f16
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-05-09 10:24:52 +0300

    Committed with the added insert and delete functions

--- Lists.py
+++ Lists.py
@@ -1,9 +1,67 @@
+from Node import Node
+
+
 class Nodes_List:
     def __init__(self):
         self.head = None
-        self.tail = None
 
-    def insertNode(self,node):
-        if self.head == None:
+
+    def insertNode(self,data):
+
+        node = Node(data)
+        if self.head is None:
             self.head = node
-            self.tail = node
+        else:
+            last = self.head
+            while last.next != None:
+                last = last.next
+
+            last.next = node
+
+        return Nodes_List
+
+    def printList(self):
+        if self.head == None:
+            print("List is empty")
+            return
+
+        else:
+            current = self.head
+            print("Linked List: ",end="")
+            while current != None:
+                print(str(current.data)+", ",end="")
+                current = current.next
+
+    def deleteNode(self, target):
+        current = self.head
+
+        if current != None and current.data == target:
+            self.head = current.next
+            return
+
+        prev = None
+        while current and current.data != target:
+            prev = current
+            current = current.next
+        if current != None:
+            prev.next = current.next
+
+
+
+
+
+def main():
+    Lists = Nodes_List()
+
+    Lists.insertNode(5)
+    Lists.insertNode(6)
+    Lists.insertNode(7)
+    Lists.insertNode(8)
+
+
+    Lists.deleteNode(5)
+    Lists.printList()
+
+
+main()
+
